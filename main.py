b54cb16660412142ae7a221396d9bf31d997bd90
GAME1.2 <GAME1.2@etpa.local>
2020-12-01 12:59:13 +0100

    Début de game of life
    Ne fonctionne pas encore pour des raisons obscure

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from os import system
+import time
+
 def isBissextile(ann):
     test="false"
     if ann%4==0 and ann%100>0:
@@ -43,8 +46,58 @@ def moyenne(list):
     moy=moy/2
     return moy
 
+def jeuDeLaVie(listCase):
+    test=0
+    for i in range(10):
+        listCase.append([0])
+        for o in range(10):
+            listCase[i].append(0)
+    # while input("Vous voulez ajouté des cases ? O/N")=="O":
+    #     ligne=int(input("ligne?"))-1
+    #     colonne=int(input("colonne?"))-1
+    #     listCase[ligne][colonne]=1
+    #     system('cls')
+    #     affichage(listCase)
+    listCase[0][0]=1
+    listCase[1][1]=1
+    listCase[1][0]=1
+    while test==0:
+        update(listCase)
+        time.sleep(1)
+        affichage(listCase)
+    
 
-listTest=[0,5,1,8,9]
-# print(isBissextile(2020))
-# print(20//4)
-print(isValidCardNumber(8763))
+def update(listCase):
+    listCopy=[]
+    for i in range(len(listCase)):
+        listCopy.append(listCase[i])
+        for o in range(len(listCase[i])):
+            listCopy[i][o]=listCase[i][o]  
+    viem=0
+    for i in range(len(listCase)):
+        for o in range(len(listCase[i])):
+            viem=0
+            for e in range(3):
+                hor=e-1
+                for f in range(3):                    
+                    vert=f-1
+                    if i+hor!=-1 and o+vert!=-1 and i+hor<len(listCopy) and o+vert<len(listCopy[i]) and listCopy[i+hor][o+vert]==1 and hor!=0 and vert!=0:
+                        viem+=1
+                        # print(viem)
+            if listCopy[i][o]==1:
+                if viem<2 or viem>3:
+                    listCase[i][o]==0
+                else:
+                    listCase[i][o]==1
+            else:
+                if viem==3:
+                    listCase[i][o]=1
+                else:
+                    listCase[i][o]=0
+
+def affichage(list):
+    system('cls')
+    for i in range(len(list)):
+        print(list[i])
+listCase=[]
+jeuDeLaVie(listCase)
